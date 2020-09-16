f975163011a9520cf440ec33c5081755eb6adb1d
Alec Kloss <akloss@cibotechnologies.com>
2020-09-16 09:03:25 -0500

    copy explicitly named labels from vault-crd to secret

--- src/main/java/de/koudingspawn/vault/Constants.java
+++ src/main/java/de/koudingspawn/vault/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm'Z'";
     public static String COMPARE_ANNOTATION = "/compare";
     public static String LAST_UPDATE_ANNOTATION = "/lastUpdated";
+    public static String COPY_LABELS_ANNOTATION = "/copyLabels";
 }
