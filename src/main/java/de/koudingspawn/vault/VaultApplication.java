07a5671dc54758dd6c6d6b36985b9083d63ffa89
Björn Wenzel <bjoern.wenzel@dbschenker.com>
2022-05-20 16:22:33 +0200

    Downgrade pkcs12 algorithm to support old java versions https://bugs.openjdk.java.net/browse/JDK-8267701

--- src/main/java/de/koudingspawn/vault/VaultApplication.java
+++ src/main/java/de/koudingspawn/vault/VaultApplication.java
@@ -1,14 +1,18 @@
 package de.koudingspawn.vault;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.security.Security;
 
 @SpringBootApplication
 @EnableScheduling
 public class VaultApplication {
 
 	public static void main(String[] args) {
+		Security.addProvider(new BouncyCastleProvider());
 		SpringApplication.run(VaultApplication.class, args);
 	}
 }
