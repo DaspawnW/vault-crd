c1925bbf6ec948935b9374d1ecfacc6e233a96b5
Björn Wenzel <bjoern.wenzel@dbschenker.com>
2023-01-31 16:06:00 +0100

    bump deprecations

--- src/main/java/de/koudingspawn/vault/kubernetes/event/EventNotification.java
+++ src/main/java/de/koudingspawn/vault/kubernetes/event/EventNotification.java
@@ -59,7 +59,7 @@ public class EventNotification {
                 .build();
 
         try {
-            client.v1().events().resource(evt).create();
+            client.v1().events().inNamespace(resource.getMetadata().getNamespace()).resource(evt).create();
         } catch (Exception ex) {
             log.error("Failed to store event for {} in namespace {} next to resource with error",
                     resource.getMetadata().getName(), resource.getMetadata().getNamespace(), ex);
